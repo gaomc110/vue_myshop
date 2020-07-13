1d5538132e389dfde07af5075ee803f274d01a78
努力努力再努力 <2029332560@qq.com>
2020-07-13 16:34:59 +0800

    完成了登录

--- src/main.js
+++ src/main.js
@@ -2,7 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+/**导入字体图标 */
+import './assets/fonts/iconfont.css'
+// 导入全局样式表
+import './assets/css/global.css'
 
+import axios from 'axios'
+Vue.prototype.$http = axios
+// 设置全局请求根路径
+axios.defaults.baseURL = 'http://119.23.53.78:8888/api/private/v1/' //案例路径
 Vue.config.productionTip = false
 
 new Vue({
