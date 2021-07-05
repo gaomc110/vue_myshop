3f1ca5f5d6807d5485098518e7995700fce38b51
gaomingcong <2029332560@qq.com>
2021-07-05 16:54:27 +0800

    提交信息

--- src/api/config.js
+++ src/api/config.js
@@ -11,7 +11,7 @@ let loading // loading
 let baseUrl = '' // 这里是一个默认的url，可以没有
 switch (process.env.NODE_ENV) {
   case 'development':
-    baseUrl = 'http://119.23.53.78:8888/api/private/v1/' // 这里是本地的请求url
+    baseUrl = 'https://www.liulongbin.top:8888/api/private/v1/' // 这里是本地的请求url
     break
   case 'production':
     baseUrl = 'http://119.23.53.78:8888/api/private/v1/' // 生产环境url
